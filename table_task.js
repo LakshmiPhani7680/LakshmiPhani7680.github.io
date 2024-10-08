71ebc7edf23e94c4982c1604395022df89d5c036
Phani Putrevu <48641068+LakshmiPhani7680@users.noreply.github.com>
2024-10-08 14:23:17 +0530

    Update table_task.js

--- table_task.js
+++ table_task.js
@@ -7,6 +7,21 @@ function submitData() {
     let age = document.getElementById("age").value;
 
     addProfileData(name, phone, email, age);
+    let table = document.getElementsByTagName("table")[0];
+      let tr = document.createElement("tr");
+      let td1 = document.createElement("td");
+      let td2 = document.createElement("td");
+      let td3 = document.createElement("td");
+      let td4 = document.createElement("td");
+      td1.innerHTML = res.name;
+      td2.innerHTML = res.phone;
+      td3.innerHTML = res.email;
+      td4.innerHTML = res.age;
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      table.appendChild(tr);
   });
 }
 
