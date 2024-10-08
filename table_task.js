2facf3f9f97a47a758c420f3b1f45479cbcbf07d
Phani Putrevu <48641068+LakshmiPhani7680@users.noreply.github.com>
2024-10-08 14:19:46 +0530

    Update table_task.js
    Added data

--- table_task.js
+++ table_task.js
@@ -33,25 +33,48 @@ function addProfileData(name, phone, email, age) {
       console.log(err);
     });
 }
-fetch("http://localhost:3000/users")
-  .then((response) => response.json())
+// fetch("http://localhost:3000/users")
+//   .then((response) => response.json())
+//   .then((result) => {
+//     result.map((res) => {
+//       let table = document.getElementsByTagName("table")[0];
+//       let tr = document.createElement("tr");
+//       let td1 = document.createElement("td");
+//       let td2 = document.createElement("td");
+//       let td3 = document.createElement("td");
+//       let td4 = document.createElement("td");
+//       td1.innerHTML = res.name;
+//       td2.innerHTML = res.phone;
+//       td3.innerHTML = res.email;
+//       td4.innerHTML = res.age;
+//       tr.appendChild(td1);
+//       tr.appendChild(td2);
+//       tr.appendChild(td3);
+//       tr.appendChild(td4);
+//       table.appendChild(tr);
+//     });
+//     submitData();
+//   });
+fetch("https://lakshmiphani7680.github.io/table_task_db.json")
   .then((result) => {
-    result.map((res) => {
-      let table = document.getElementsByTagName("table")[0];
-      let tr = document.createElement("tr");
-      let td1 = document.createElement("td");
-      let td2 = document.createElement("td");
-      let td3 = document.createElement("td");
-      let td4 = document.createElement("td");
-      td1.innerHTML = res.name;
-      td2.innerHTML = res.phone;
-      td3.innerHTML = res.email;
-      td4.innerHTML = res.age;
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-      tr.appendChild(td3);
-      tr.appendChild(td4);
-      table.appendChild(tr);
+    result.json().then((result) => {
+      result.users.map((res) => {
+        let table = document.getElementsByTagName("table")[0];
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        td1.innerHTML = res.name;
+        td2.innerHTML = res.phone;
+        td3.innerHTML = res.email;
+        td4.innerHTML = res.age;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        table.appendChild(tr);
+      });
     });
     submitData();
   });
