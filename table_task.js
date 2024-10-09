067f5276e9978ff08a57e886c8e7cae8d831dd5f
Phani Putrevu <48641068+LakshmiPhani7680@users.noreply.github.com>
2024-10-09 12:32:24 +0530

    Changed to localStorage from db

--- table_task.js
+++ table_task.js
@@ -1,95 +1,97 @@
-function submitData() {
-  document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    let phone = document.getElementById("phone").value;
-    let email = document.getElementById("email").value;
-    let age = document.getElementById("age").value;
+let usersData = JSON.parse(localStorage.getItem("usersData")) || [
+  {
+    id: 1,
+    name: "Phani",
+    phone: 9782564723,
+    email: "phani@gmail",
+    age: 22,
+    gender: "Male",
+  },
+  {
+    id: 2,
+    name: "Lakshmi",
+    phone: 9772382564,
+    email: "lakshmi@gmail",
+    age: 21,
+    gender: "Female",
+  },
+  {
+    id: 3,
+    name: "Gana",
+    phone: 9782235647,
+    email: "gana@gmail",
+    age: 24,
+    gender: "Male",
+  },
+];
 
-    addProfileData(name, phone, email, age);
-    let table = document.getElementsByTagName("table")[0];
-      let tr = document.createElement("tr");
-      let td1 = document.createElement("td");
-      let td2 = document.createElement("td");
-      let td3 = document.createElement("td");
-      let td4 = document.createElement("td");
-      td1.innerHTML = name;
-      td2.innerHTML = phone;
-      td3.innerHTML = email;
-      td4.innerHTML = age;
-      tr.appendChild(td1);
-      tr.appendChild(td2);
-      tr.appendChild(td3);
-      tr.appendChild(td4);
-      table.appendChild(tr);
-  });
-}
+document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let age = document.getElementById("age").value;
+  let gender = document.getElementById("gender").value;
 
-function addProfileData(name, phone, email, age) {
   let data = {
+    id: Math.floor(Math.random() * 1000) + 1,
     name: name,
     phone: phone,
     email: email,
     age: age,
+    gender: gender,
   };
-  console.log(data);
-  let options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-  fetch("https://lakshmiphani7680.github.io/table_task_db.json", options)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  console.table(data);
+  usersData.push(data);
+  localStorage.setItem("usersData", JSON.stringify(usersData));
+
+  renderTable();
+  document.getElementById("form").reset();
+});
+
+function getData() {
+  let storedData = localStorage.getItem("usersData");
+  if (storedData) {
+    usersData = JSON.parse(storedData);
+  }
+  renderTable();
 }
-// fetch("http://localhost:3000/users")
-//   .then((response) => response.json())
-//   .then((result) => {
-//     result.map((res) => {
-//       let table = document.getElementsByTagName("table")[0];
-//       let tr = document.createElement("tr");
-//       let td1 = document.createElement("td");
-//       let td2 = document.createElement("td");
-//       let td3 = document.createElement("td");
-//       let td4 = document.createElement("td");
-//       td1.innerHTML = res.name;
-//       td2.innerHTML = res.phone;
-//       td3.innerHTML = res.email;
-//       td4.innerHTML = res.age;
-//       tr.appendChild(td1);
-//       tr.appendChild(td2);
-//       tr.appendChild(td3);
-//       tr.appendChild(td4);
-//       table.appendChild(tr);
-//     });
-//     submitData();
-//   });
-fetch("https://lakshmiphani7680.github.io/table_task_db.json")
-  .then((result) => {
-    result.json().then((result) => {
-      result.users.map((res) => {
-        let table = document.getElementsByTagName("table")[0];
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        let td2 = document.createElement("td");
-        let td3 = document.createElement("td");
-        let td4 = document.createElement("td");
-        td1.innerHTML = res.name;
-        td2.innerHTML = res.phone;
-        td3.innerHTML = res.email;
-        td4.innerHTML = res.age;
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-        table.appendChild(tr);
-      });
-    });
-    submitData();
+
+function renderTable() {
+  let table = document.getElementsByTagName("table")[0];
+
+  table.innerHTML = `<tr>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>Age</th>
+      <th>Gender</th>
+    </tr>`;
+
+  usersData.map((res) => {
+    let tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+
+    td1.innerText = res.name;
+    td2.innerText = res.phone;
+    td3.innerText = res.email;
+    td4.innerText = res.age;
+    td5.innerText = res.gender;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+
+    table.appendChild(tr);
   });
+}
+
+getData();
